c2b9a612bc9e12b3be9c4c03e8f68801c115dd7a
RownakDip <52921729+RownakDip@users.noreply.github.com>
2020-12-23 07:07:02 +0600

    Enhancement #9 : Fixing Folder Structure

--- Code/Practice/app/src/main/java/com/example/gesturecontrol/MainActivity.java
+++ Code/Practice/app/src/main/java/com/example/gesturecontrol/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.gesturecontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accessibilityservice.FingerprintGestureController;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureControl.Listener
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
