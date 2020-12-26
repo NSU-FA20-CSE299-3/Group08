6d5aa171db51fb9d0c73e60126d544ed821d6712
RownakDip <52921729+RownakDip@users.noreply.github.com>
2020-12-26 23:27:45 +0600

    Enhancement #11 : UI updated

--- Code/Practice/CallApp/app/src/main/java/com/Ric/App/MainActivity.java
+++ Code/Practice/CallApp/app/src/main/java/com/Ric/App/MainActivity.java
@@ -18,12 +18,10 @@ public class MainActivity extends android.app.Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.buttonCall);
-
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
                 Intent callIntent = new Intent(Intent.ACTION_CALL);
                 callIntent.setData(Uri.parse("tel:0377778888"));
-
                 if (ActivityCompat.checkSelfPermission(MainActivity.this,
                         Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
                     return;
