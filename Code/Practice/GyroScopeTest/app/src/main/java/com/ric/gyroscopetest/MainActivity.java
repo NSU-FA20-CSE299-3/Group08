6fd579966e1e51565191282c75ef7a4ca0a46681
RownakDip <52921729+RownakDip@users.noreply.github.com>
2020-12-29 21:41:56 +0600

    Enhancement #14 : attributes and sensores defined.

--- Code/Practice/GyroScopeTest/app/src/main/java/com/ric/gyroscopetest/MainActivity.java
+++ Code/Practice/GyroScopeTest/app/src/main/java/com/ric/gyroscopetest/MainActivity.java
@@ -2,13 +2,27 @@ package com.ric.gyroscopetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity
+{
+    private TextView x,y,z;
+    private SensorManager sensorManager;
+    private Sensor accelorometter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        x = findViewById(R.id.x);
+        y= findViewById(R.id.y);
+        z= findViewById(R.id.z);
+
     }
 }
