b7b9b94519e6479da9b23613803dbd89e675b43c
RownakDip <52921729+RownakDip@users.noreply.github.com>
2021-01-05 19:42:41 +0600

    Enhancement #16 : SMS reciving with location successful

--- Code/Project/app/src/main/java/com/example/lifesaverric/Register.java
+++ Code/Project/app/src/main/java/com/example/lifesaverric/Register.java
@@ -1,8 +1,9 @@
-package com.example.myapplication;
+package com.example.lifesaverric;
+
+
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -11,7 +12,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -22,16 +22,16 @@ public class Register extends AppCompatActivity
 
 {
     EditText mFullName,mEmail,mPassword,mPhone;
-            Button mRegisterbtn;
-            TextView mLoginBtn;
-            FirebaseAuth fAuth;
+    Button mRegisterbtn;
+    TextView mLoginBtn;
+    FirebaseAuth fAuth;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register2);
+        setContentView(R.layout.activity_register);
 
         mFullName = findViewById(R.id.fullname);
         mEmail = findViewById(R.id.email);
@@ -43,27 +43,31 @@ public class Register extends AppCompatActivity
         fAuth = FirebaseAuth.getInstance();
         if (fAuth.getCurrentUser() != null)
         {
-            startActivity(new Intent(getApplicationContext(),MainActivity.class));
-              finish();
+            startActivity(new Intent(getApplicationContext(),MapsActivity.class));
+            finish();
         }
 
         mRegisterbtn.setOnClickListener(new View.OnClickListener()
-    {
+        {
             @Override
             public void onClick(View v)
             {
                 String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
 
+
+
+
+
                 if (TextUtils.isEmpty(email)) {
                     mEmail.setError("Email is Required");
                     return;
                 }
 
-                 if(TextUtils.isEmpty(password)) {
-                     mPassword.setError("Password is required");
-                     return;
-                 }
+                if(TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is required");
+                    return;
+                }
 
                 if (password.length()<8)
                 {
@@ -76,7 +80,7 @@ public class Register extends AppCompatActivity
                         if (task.isSuccessful())
                         {
                             Toast.makeText(Register.this, "User Created", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                            startActivity(new Intent(getApplicationContext(),MapsActivity.class));
                         }
                         else
                         {
@@ -89,14 +93,14 @@ public class Register extends AppCompatActivity
 
 
         });
-                       mLoginBtn.setOnClickListener(new View.OnClickListener() {
-                           @Override
-                           public void onClick(View v) {
-                               startActivity(new Intent(getApplicationContext(),Login.class));
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Login.class));
 
 
-                           }
-                       });
+            }
+        });
 
     }
 }
